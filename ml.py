00c9889973c796edf83490a54ebb3bb9aaba8aa6
Nathan Marquez <nathanmarquez@Nathans-MacBook-Pro.local>
2020-11-07 12:04:25 -0500

    Updated Ml to read from CSV (delete this line and write in the main course)

--- ml.py
+++ ml.py
@@ -1,37 +1,30 @@
+import csv
+import pandas as pd
+from csv import reader
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn import linear_model
 
 def main():
-            #test log
-            #Course HW Notes Zoom Read | Grade    
-    courseLog = [['15112',.02,.90,.04,.60,.10],
-                ['21241',.20,.30,.40,.10,.50],
-                ['07128',.10,.20,.50,.20,.60],
-                ['15150',.30,.30,.30,.10,.40],
-                ['21242',.90,.10,.70,.30,.80],
-                ['08781',.70,.30,.20,.10,.30]]
+    datafile = open('courseLog.csv', 'r')
+    datareader = csv.reader(datafile, delimiter=',')
+    courseLog = []
+    for row in datareader:
+        courseLog.append(row)    
 
-    course0Model = initialTrain(0, courseLog)
+    course0Model = initialTrain(0,courseLog)
     print("initialModelWeight:",course0Model.coef_)
-    courseLog[0][2] = .7 
-    courseLog[0][3] = .3
-    courseLog[0][5] = .72
-    updateModel(0,courseLog,course0Model)
-    print(course0Model.coef_)
-    courseLog[0][2] = .8 
-    courseLog[0][3] = .1
-    courseLog[0][5] = .82
-    updateModel(0,courseLog,course0Model)
-    print(course0Model.coef_)
 
-def getVector(course, courseLog):
+
+#Helper Function to get training vectors for other functions
+def getVector(course,courseLog):
     grade = courseLog[course][5]
     x = [courseLog[course][1:5]]
     y = [grade]
     return x,y
 
-def initialTrain(course, courseLog): #returns new model from all initial data
+#returns new model from all initial data, only ran at setup
+def initialTrain(course,courseLog): 
     x = []
     for row in courseLog:
         current = []
@@ -46,7 +39,8 @@ def initialTrain(course, courseLog): #returns new model from all initial data
     sgd.fit(x, y)
     return sgd
 
-def updateModel(course,courseLog, model): #destructive, updates model
+#destructive, updates model everytime a grade is updated
+def updateModel(course, model, courseLog): 
     x,y = getVector(course, courseLog)
     model.partial_fit(x, y)
 
